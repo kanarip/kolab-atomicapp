b655495f8f577c4e0b806453f161a7a6a29d9db3
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-10-30 10:03:08 +0100

    Fix syntax error, mount /data hopefully

--- 99-imap/entrypoint.sh
+++ 99-imap/entrypoint.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# Do something actual here.
 if [ ! -f "/etc/imapd.conf" ]; then
     setup-kolab --default imap
 fi
+
+persist=(
+    /etc/pki/tls/certs/
+    /etc/pki/tls/private/
+    /var/lib/imap/
+    /var/spool/imap/
+)
+
+for p in ${persist[@]}; do
+    if [ -d "${p}" ]; then
+        mkdir -p /data$(dirname ${p})
+        mv ${p} /data$(dirname ${p})
+        ln -s /data${p} ${p}
+    fi
+done
 
 systemctl start kolab-saslauthd
 
