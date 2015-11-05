519391653c6de9f4e8a451643cc6bb4ca87b4b02
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-05 14:27:18 +0100

    Amavis needs a configuration file, do not exec clamav

--- 01-base-asav/entrypoint.sh
+++ 01-base-asav/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-exec su -s /bin/bash - amavis -c 'nohup /usr/sbin/clamd -c /etc/clamd.d/amavisd.conf --pid /var/run/amavisd/clamd.pid &'
+su -s /bin/bash - amavis -c 'nohup /usr/sbin/clamd -c /etc/clamd.d/amavisd.conf --pid /var/run/amavisd/clamd.pid &'
 
 if [ -z "$@" ]; then
-    exec /usr/sbin/amavisd -u amavis -g amavis foreground
+    exec /usr/sbin/amavisd \
+        -u amavis \
+        -g amavis \
+        -c /etc/amavisd/amavisd.conf \
+        foreground
 else
     exec "$@"
 fi
