cc42f5ae59951c7f86ab482d59316eafc84c9940
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-06 08:15:23 +0100

    Amavis is very specific about its configured and/or system FQDN

--- 01-base-asav/entrypoint.sh
+++ 01-base-asav/entrypoint.sh
@@ -2,6 +2,17 @@
 
 su -s /bin/bash - amavis -c 'nohup /usr/sbin/clamd -c /etc/clamd.d/amavisd.conf --pid /var/run/amavisd/clamd.pid &'
 
+if [ -z "${DOMAIN}" -o "${DOMAIN}" == "" ]; then
+    DOMAIN="docker.container"
+fi
+
+if [ -z "$(hostname | awk -F'.' '{print $3}')" -a ! -z "${DOMAIN}" ]; then
+    sed -r -i \
+        -e "s/^\\\$mydomain = .*$/\$mydomain = '${DOMAIN}';/g" \
+        -e "s/^# \\\$myhostname.*$/\$myhostname = '$(hostname).${DOMAIN}';/g" \
+        /etc/amavisd/amavisd.conf
+fi
+
 if [ $# -lt 1 ]; then
     exec /usr/sbin/amavisd \
         -u amavis \
