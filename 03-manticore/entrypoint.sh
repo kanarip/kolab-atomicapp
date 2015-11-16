e14bc423ed5a2f24374337dbfc5b0ef594dec2ee
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-16 17:28:33 +0100

    Add settings to create the initial admin account (upload templates and fonts)

--- 03-manticore/entrypoint.sh
+++ 03-manticore/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+sed -i \
+    -e "s/email: 'admin@admin.com',/email: '${MONGODB_ADMIN_EMAIL:-admin@admin.com}',/g" \
+    -e "s/password: 'admin',/password: '${MONGODB_ADMIN_PASSWORD:-password}'/g" \
+    /var/www/manticore.git/server/config/seed.js
+
 export MONGOLAB_URI="mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_SERVICE_HOST}/${MONGODB_DATABASE}"
 
 exec "$@"
