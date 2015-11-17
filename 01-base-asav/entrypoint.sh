440b28d5e354757bcd0af2656fc937d66da93ebe
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-17 15:49:31 +0100

    Ensure the entrypoint takes in to account the $KOLAB_ROLE environment variable, such that they can find their appropriate forward_method

--- 01-base-asav/entrypoint.sh
+++ 01-base-asav/entrypoint.sh
@@ -2,6 +2,8 @@
 
 su -s /bin/bash - amavis -c 'nohup /usr/sbin/clamd -c /etc/clamd.d/amavisd.conf --pid /var/run/amavisd/clamd.pid &'
 
+env
+
 if [ -z "${DOMAIN}" -o "${DOMAIN}" == "" ]; then
     DOMAIN="docker.container"
 fi
@@ -10,6 +12,18 @@ if [ -z "$(hostname | awk -F'.' '{print $3}')" -a ! -z "${DOMAIN}" ]; then
     sed -r -i \
         -e "s/^\\\$mydomain = .*$/\$mydomain = '${DOMAIN}';/g" \
         -e "s/^# \\\$myhostname.*$/\$myhostname = '$(hostname).${DOMAIN}';/g" \
+        /etc/amavisd/amavisd.conf
+fi
+
+if [ "${KOLAB_ROLE}" == "ASAV_IN" ]; then
+    FORWARD_METHOD="smtp:[${KOLAB_EXT_MX_IN_SERVICE_HOST}]:10025"
+elif [ "${KOLAB_ROLE}" == "ASAV_OUT" ]; then
+    FORWARD_METHOD="smtp:[${KOLAB_EXT_MX_OUT_SERVICE_HOST}]:10025"
+fi
+
+if [ ! -z "${FORWARD_METHOD}" ]; then
+    sed -r -i \
+        -e "s/^# \\\$forward_method.*$/\$forward_method = '${FORWARD_METHOD}';/g" \
         /etc/amavisd/amavisd.conf
 fi
 
