84a0b5af2c59a6c20717b25debe64f9f0e63e93e
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-17 15:51:18 +0100

    Link up ext-mx's and asav's

--- 03-ext-mx-in/entrypoint.sh
+++ 03-ext-mx-in/entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+env
+
 if [ ! -d "/etc/postfix/ldap/" ]; then
-    setup-kolab --default mta
+    cp /etc/kolab/kolab.conf /root/kolab.conf
+
+    # Subst. some config settings here.
+
+    setup-kolab --config /root/kolab.conf mta
     systemctl stop postfix
 fi
 
-systemctl start kolab-saslauthd
+systemctl stop kolab-saslauthd
+
+postconf -e "content_filter=smtp:[${KOLAB_ASAV_IN_SERVICE_HOST}]:${KOLAB_ASAV_IN_SERVICE_PORT}"
 
 # Apply some configuration
 exec "$@"
--- 03-ext-mx-out/entrypoint.sh
+++ 03-ext-mx-out/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+env
+
 if [ ! -d "/etc/postfix/ldap/" ]; then
     setup-kolab --default mta
     systemctl stop postfix
