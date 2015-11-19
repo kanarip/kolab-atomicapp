a5cc5f295565da4addf712be999a3e8f08dd900d
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-19 16:09:35 +0100

    Supply a set of commonly used functions

---
+++ 00-base/functions.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+exec 3>&1
+
+function check_var() {
+    if [ -z "$(eval "echo \$$1")" ]; then
+        echo "$1 not defined."
+        return 1
+    fi
+
+    return 0
+}
+
+function check_vars() {
+    retval=0
+
+    while [ $# -gt 0 ]; do
+        retval=$(( $(check_var $1 >&3; echo $?) + ${retval} ))
+        shift
+    done
+
+    return ${retval}
+}
+
+function domain_to_root_dn() {
+    echo "dc=$(echo $1 | sed -e 's/\./,dc=/g')"
+}
+
+function persist() {
+    while [ $# -gt 0 ]; do
+        if [ ! -d "/data$(dirname $1)" ]; then
+            mkdir -p "/data$(dirname $1)"
+        fi
+
+        mv -v $1 "/data$(dirname $1)/."
+        ln -svf "/data$1" "$(dirname $1)/$(basename $1)"
+
+        shift
+    done
+}
