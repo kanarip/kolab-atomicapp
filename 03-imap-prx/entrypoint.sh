19eae27d745b556911143823290a8cc43b6e868b
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-21 15:24:29 +0100

    Update imap-prx

--- 03-imap-prx/entrypoint.sh
+++ 03-imap-prx/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-exec "$@"
+. /functions.sh
 
-exec top
+check_var \
+    KOLAB_IMAPF_EXT_SERVICE_HOST \
+    KOLAB_IMAPF_EXT_SERVICE_PORT \
+    || exit 1
+
+sed -r -i \
+    -e "s/host, \"192\.168\.56\.101\"/host, \"${KOLAB_IMAPF_EXT_SERVICE_HOST}\"/g" \
+    -e "s/port, 143/port, ${KOLAB_IMAPF_EXT_SERVICE_PORT}/g" \
+    -e "s/tls, false/tls, true/g" \
+    /root/guam.git/app.config
+
+exec "$@"
