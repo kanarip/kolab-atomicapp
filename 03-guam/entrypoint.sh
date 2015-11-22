92b912ea6219f69d91665ac57ae5391b5242f891
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-22 10:45:37 +0100

    The port is always the same, really

--- 03-guam/entrypoint.sh
+++ 03-guam/entrypoint.sh
@@ -4,12 +4,10 @@
 
 check_var \
     KOLAB_IMAPF_EXT_SERVICE_HOST \
-    KOLAB_IMAPF_EXT_SERVICE_PORT \
     || exit 1
 
 sed -r -i \
     -e "s/host, \"192\.168\.56\.101\"/host, \"${KOLAB_IMAPF_EXT_SERVICE_HOST}\"/g" \
-    -e "s/port, 143/port, ${KOLAB_IMAPF_EXT_SERVICE_PORT}/g" \
     -e "s/tls, false/tls, true/g" \
     /root/guam.git/app.config
 
