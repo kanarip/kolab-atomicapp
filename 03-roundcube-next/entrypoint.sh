cfa0af929e5331699262fd173a6c658636269cc2
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-29 12:10:42 +0100

    Use the entrypoint to hook up kubernetes configuration and regularly running a JMAP proxy

--- 03-roundcube-next/entrypoint.sh
+++ 03-roundcube-next/entrypoint.sh
@@ -2,4 +2,10 @@
 
 . /functions.sh
 
+check_var KOLAB_JMAP_PROXY_SERVICE_HOST || check_var JMAP_HOST || exit 1
+
+if [ ! -z "${KOLAB_JMAP_PROXY_SERVICE_HOST}" ]; then
+    export JMAP_HOST="http://${KOLAB_JMAP_PROXY_SERVICE_HOST}:${KOLAB_JMAP_PROXY_SERVICE_PORT:-80}"
+fi
+
 exec "$@"
