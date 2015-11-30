15efd1bc877fb31cd16d5958732ea60d217782e4
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-11-30 09:22:15 +0100

    Correct guam entrypoint to use the correct configuration file

--- 03-guam/entrypoint.sh
+++ 03-guam/entrypoint.sh
@@ -9,6 +9,6 @@ check_var \
 sed -r -i \
     -e "s/host, \"192\.168\.56\.101\"/host, \"${KOLAB_IMAPF_EXT_SERVICE_HOST}\"/g" \
     -e "s/tls, false/tls, true/g" \
-    /root/guam.git/rel/kolab_guam/releases/0.1/sys.config
+    /etc/guam/sys.config
 
 exec "$@"
