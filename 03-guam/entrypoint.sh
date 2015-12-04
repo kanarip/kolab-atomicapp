50436663e9210f85c08c2e544c3283ed9e70ddec
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-12-04 09:54:47 +0100

    Ensure a certificate is created. Use the certificate at run-time

--- 03-guam/entrypoint.sh
+++ 03-guam/entrypoint.sh
@@ -6,9 +6,16 @@ check_var \
     KOLAB_IMAPF_EXT_SERVICE_HOST \
     || exit 1
 
+if [ ! -f "/etc/pki/tls/private/localhost.pem" ]; then
+    pushd /etc/pki/tls/certs/
+    ./make-dummy-cert ../private/localhost.pem
+    popd
+fi
+
 sed -r -i \
     -e "s/host, \"192\.168\.56\.101\"/host, \"${KOLAB_IMAPF_EXT_SERVICE_HOST}\"/g" \
     -e "s/tls, false/tls, true/g" \
+    -e "s|/etc/ssl/sample.pem|/etc/pki/tls/private/localhost.pem|g" \
     /etc/guam/sys.config
 
 exec "$@"
